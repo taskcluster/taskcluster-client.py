8fd49ca1eb27c524df0b4617bb821111651d0f47
John Ford <john@johnford.org>
2016-04-18 18:40:17 +0200

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 # The VERSION variable is automagically changed
 # by release.sh.  Make sure you understand how
 # that script works if you want to change this
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 
 tests_require = [
